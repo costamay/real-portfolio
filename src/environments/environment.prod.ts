873de1b74d48140303211be329c2bf652bc15c93
Titus Ouko <titusouko@gmail.com>
2019-12-19 14:16:45 +0300

    Add fonsize to the about paragraphs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  accesstoken:"94af8fc63f808eb3158dd65ea1a4598ff80e0dba",
+  GITAPIKEY: "94af8fc63f808eb3158dd65ea1a4598ff80e0dba"
 };
